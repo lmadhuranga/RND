83d45786ae566f8905806037fb44efc0d7250514
lmadhuranga <lilan.maduranga@gmail.com>
2020-06-04 14:30:22 +0530

    added socket exaple

---
+++ nodej_socketio/server.js
@@ -0,0 +1,32 @@
+const express = require('express'); // using express 
+const socketIO = require('socket.io'); 
+const http = require('http')  
+const port = process.env.PORT||3000 // setting the port  
+let app = express(); 
+let server = http.createServer(app) 
+let io = socketIO(server) 
+
+// make connection with user from server side 
+io.on('connection', (socket)=>{ 
+  console.log('New user connected'); 
+  //emit message from server to user 
+  socket.emit('newMessage', { 
+    from:'jen@mds', 
+    text:'hepppp', 
+    createdAt:123 
+  }); 
+
+  // listen for message from user 
+  socket.on('createMessage', (newMessage)=>{ 
+    console.log('newMessage', newMessage); 
+     // sending to all clients except sender
+    socket.broadcast.emit('broadcast', 'hello friends!');
+  }); 
+
+  // when server disconnects from user 
+  socket.on('disconnect', ()=>{ 
+    console.log('disconnected from user'); 
+  }); 
+}); 
+
+server.listen(port); 
