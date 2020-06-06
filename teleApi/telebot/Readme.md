f220138742eb0c0813e5ddb034abc805098aecef
lmadhuranga <lilan.maduranga@gmail.com>
2020-06-06 08:14:19 +0530

    added link with telegram api

--- teleApi/telebot/Readme.md
+++ teleApi/telebot/Readme.md
@@ -1,1 +1,13 @@
+HOw to create a simple bot
 https://github.com/mullwar/telebot#sendmessagechat_id-text-parsemode-replytomessage-replymarkup-notification-webpreview
+
+Send message to telgram bot
+https://www.youtube.com/watch?v=UhZtrhV7t3U&feature=youtu.be
+https://dev.to/rizkyrajitha/get-notifications-with-telegram-bot-537l
+
+1.Create the bot 
+2. Create group 
+3.Add the bot to telgram group 
+
+4.Put the group id in the chat id and send (Create g339277497 => remove g and add - to prefix  )
+g339277497 -> -339277497
--- teleApi/telebot/server.js
+++ teleApi/telebot/server.js
@@ -1,25 +1,25 @@
 const TeleBot = require('telebot');
 const bot = new TeleBot('1233602346:AAHtgDal0o6IlGlxz0LMdsO5dDTmNY5uil4');
-bot.on('text', (msg) => {
-  console.log('msg',msg);
-  msg.reply.text(msg.text)
-});
 
 bot.on(['/start', '/hello'], (msg) => msg.reply.text('Welcome!'));
-// chat_id: number | string,
-//         text: string,
-//         opt?: {
-//             parseMode?: string;
-//             replyToMessage?: number;
-//             replyMarkup?: any;
-//             notification?: boolean;
-//             webPreview?: boolean;
-//         }
-// bot.sendMessage(<chat_id>, <text>, {parseMode, replyToMessage, replyMarkup, notification, webPreview})
-bot.on(/(show\s)?kitty*/, (msg) => {
-  return msg.reply.photo('http://thecatapi.com/api/images/get');
-});
+
+bot.mod('text', (data) => {
+  let msg = data.message;
+  msg.text = `📢 ${ msg.text }`;
+  return data;
+}); 
+
 bot.on('/pic', (msg) => {
   return msg.reply.photo('http://thecatapi.com/api/images/get');
 });
+bot.on('sticker', (msg) => {
+  return msg.reply.sticker('http://i.imgur.com/VRYdhuD.png', { asReply: true });
+});
+
+bot.on(['/hellog'], (msg) => bot.sendMessage('-339277497', 'bot to group'));
+
+
+bot.on(['/helloc'], (msg) => console.log(msg));
+
+
 bot.start();
